3392b408dc9f980102252a36492a7c40c7c252a2
Viviane Marty <vivianemarty@Vivianes-MacBook-Pro.local>
2019-09-28 19:24:21 +0200

    one more try

---
+++ HackZurich2019/graphics/ProgressState.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressState : MonoBehaviour
+{
+    public ProgressBar Pb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Pb.BarValue = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCO2CostReceived()
+
+}
